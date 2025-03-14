69e61fa59b4f94365b1a07ad687516117a61a741
lagrot <89245440+lagrot@users.noreply.github.com>
2025-03-14 13:56:22 +0100

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,10 @@
+# Alias's for multiple directory listing commands
+alias ls='ls -aFh --color=always'
+alias ll='ls -Fls --color=always' # long listing format
+alias la='ls -Alh --color=always' # show hidden files
+
+# Show open ports
+alias openports='netstat -nape --inet'
+
+#Div
+alias vi='vim'
